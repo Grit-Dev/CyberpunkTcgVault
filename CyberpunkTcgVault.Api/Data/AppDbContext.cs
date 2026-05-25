b382de619562bfb61e5c724d93f0c724a90d750c
Paul McGinley <paulmcginley16@outlook.com>
2026-05-25 18:21:22 +0100

    Add EF core database contect

---
+++ CyberpunkTcgVault.Api/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CyberpunkTcgVault.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CyberpunkTcgVault.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+            //PMG TODO: Db provider to be configured and connection string being used. 
+        }
+
+        public DbSet<Card> Cards { get; set; }
+    }
+}
