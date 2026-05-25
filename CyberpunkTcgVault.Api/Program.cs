e088e716d87b28f1577b6b877187b0e1e772b1c4
Paul McGinley <paulmcginley16@outlook.com>
2026-05-25 18:29:31 +0100

    confiured EF core SQl Connection

--- CyberpunkTcgVault.Api/Program.cs
+++ CyberpunkTcgVault.Api/Program.cs
@@ -1,8 +1,16 @@
+using CyberpunkTcgVault.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Registers AppDbContext so the API can connect to SQL Server using the DefaultConnection string.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
