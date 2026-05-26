df5b644dab82d173351e0cfe6ffa8855c338e2ad
Paul McGinley <paulmcginley16@outlook.com>
2026-05-26 18:43:08 +0100

    Add validation to create card request DTO

--- CyberpunkTcgVault.Api/DTOs/CreateCardRequest.cs
+++ CyberpunkTcgVault.Api/DTOs/CreateCardRequest.cs
@@ -1,25 +1,38 @@
-﻿namespace CyberpunkTcgVault.Api.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CyberpunkTcgVault.Api.DTOs
 {
     public class CreateCardRequest
     {
+        [Required]
+        [StringLength(200)]
         public string Name { get; set; } = string.Empty;
 
+        [StringLength(200)]
         public string? SetName { get; set; }
 
+        [StringLength(50)]
         public string? Rarity { get; set; }
 
+        [StringLength(50)]
         public string? Colour { get; set; }
 
+        [StringLength(50)]
         public string? CardType { get; set; }
 
+        [StringLength(100)]
         public string? Classification { get; set; }
 
+        [StringLength(250)]
         public string? Keywords { get; set; }
 
+        // PMG TODO: Will come back to this.
         public int? Cost { get; set; }
 
+        [Range(0, 50)]
         public int? Power { get; set; }
 
+        [Range(0, 50)]
         public int? RamCost { get; set; }
 
         public bool IsLegend { get; set; }
@@ -40,10 +53,14 @@
 
         public bool IsStarterDeckExclusive { get; set; }
 
+        // PMG TODO: Need to consider next new sets that come out
+        [StringLength(200)
         public string? CardNumber { get; set; }
 
+        [StringLength(500)]
         public string? ImageUrl { get; set; }
 
+        [StringLength(2000)]
         public string? Notes { get; set; }
     }
 }
