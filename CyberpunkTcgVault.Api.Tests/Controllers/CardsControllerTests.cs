409a5cdad0865c0282546d016fa697813c6f14b0
Paul McGinley <paulmcginley16@outlook.com>
2026-07-02 19:18:20 +0100

    Test: Gets Cards When Cards Exist_ReturnOkWithTheCards

--- CyberpunkTcgVault.Api.Tests/Controllers/CardsControllerTests.cs
+++ CyberpunkTcgVault.Api.Tests/Controllers/CardsControllerTests.cs
@@ -1,6 +1,49 @@
-﻿namespace CyberpunkTcgVault.Api.Tests.Controllers
+﻿using CyberpunkTcgVault.Api.Controllers;
+using CyberpunkTcgVault.Api.Data;
+using CyberpunkTcgVault.Api.Models;
+using CyberpunkTcgVault.Api.Tests.TestHelpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace CyberpunkTcgVault.Api.Tests.Controllers
 {
     public class CardsControllerTests
     {
+        private static CardsController CreateCardsController(AppDbContext context)
+        {
+            return new CardsController(context, NullLogger<CardsController>.Instance);
+        }
+
+        [Fact]
+        public async Task GetCards_WhenCardsExist_ReturnOkWithTheCards()
+        {
+            // Arrange
+            var context = TestDbContextFactory.Create();
+
+            context.Cards.Add(new Models.Card
+            {
+                Name = "Johnny Silverhand",
+                SetName = "Beta",
+                Rarity = "Rare",
+                Colour = "Red",
+                CardType = "Legend"
+
+            });
+
+            await context.SaveChangesAsync();
+
+            var controller = CreateCardsController(context);
+
+            // Act
+            var result = await controller.GetCards();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+
+            var cards = Assert.IsAssignableFrom<IEnumerable<Card>>(okResult.Value);
+
+            Assert.Contains(cards, card => card.Name == "Johnny Silverhand");
+
+        }
     }
 }
