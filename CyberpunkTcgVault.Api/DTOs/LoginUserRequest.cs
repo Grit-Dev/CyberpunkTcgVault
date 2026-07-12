86a830841f98b939c27bae2a877243a6b311dd96
Paul McGinley <paulmcginley16@outlook.com>
2026-07-12 11:17:15 +0100

    RegistedUserRquest and LoginUserReques in the DTO

---
+++ CyberpunkTcgVault.Api/DTOs/LoginUserRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CyberpunkTcgVault.Api.DTOs
+{
+    public class LoginUserRequest
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ CyberpunkTcgVault.Api/DTOs/RegisterUserRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CyberpunkTcgVault.Api.DTOs
+{
+    public class RegisterUserRequest
+    {
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50, MinimumLength = 6)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
