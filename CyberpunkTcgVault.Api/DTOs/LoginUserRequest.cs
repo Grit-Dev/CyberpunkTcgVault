95fba996e352a31bb4bda21c11c71e0d86a581ea
Paul McGinley <paulmcginley16@outlook.com>
2026-07-12 11:19:45 +0100

    Add StringLength to user Login

--- CyberpunkTcgVault.Api/DTOs/LoginUserRequest.cs
+++ CyberpunkTcgVault.Api/DTOs/LoginUserRequest.cs
@@ -5,9 +5,11 @@ namespace CyberpunkTcgVault.Api.DTOs
     public class LoginUserRequest
     {
         [Required]
+        [StringLength(50)]
         public string UserName { get; set; } = string.Empty;
 
         [Required]
+        [StringLength(50)]
         public string Password { get; set; } = string.Empty;
     }
 }
