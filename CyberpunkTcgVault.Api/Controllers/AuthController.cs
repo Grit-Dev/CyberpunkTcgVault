d9c7733c281862a89ce25c90a1c2379e0ee1e086
Paul McGinley <paulmcginley16@outlook.com>
2026-07-12 11:41:29 +0100

    Add user Reg

---
+++ CyberpunkTcgVault.Api/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using CyberpunkTcgVault.Api.Data;
+using CyberpunkTcgVault.Api.DTOs;
+using CyberpunkTcgVault.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CyberpunkTcgVault.Api.Controllers
+
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+        private readonly IPasswordHasher<AppUser> _passwordHasher;
+
+        public AuthController(AppDbContext context, IPasswordHasher<AppUser> passwordHasher)
+        {
+            _context = context;
+            _passwordHasher = passwordHasher;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterUserRequest request)
+        {
+            var userExists = await _context.Users
+                .AnyAsync(user => user.UserName == request.UserName);
+
+            if (userExists)
+            {
+                return Conflict("Username already exists.");
+            }
+
+            var user = new AppUser
+            {
+                UserName = request.UserName.Trim()
+            };
+
+            // No Password Saved - Its hashed and we only get back the Hash!
+            user.PasswordHash = _passwordHasher.HashPassword(
+                user,
+                request.Password);
+
+            _context.Users.Add(user);
+
+            await _context.SaveChangesAsync();
+
+            return StatusCode(
+                StatusCodes.Status201Created,
+                new { message = "User registered successfully." });
+        }
+    }
+}
