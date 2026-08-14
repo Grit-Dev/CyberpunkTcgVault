85821d4a1797f883e896ceb3c9c1d66a20aeb95b
Paul McGinley <paulmcginley16@outlook.com>
2026-08-14 20:59:04 +0100

    feat: handle duplicate wishlist conflicts

---
+++ CyberpunkTcgVault.Api/Services/Results/WishListItemCreateStatus.cs
@@ -0,0 +1,19 @@
+﻿using CyberpunkTcgVault.Api.DTOs;
+
+namespace CyberpunkTcgVault.Api.Services.Results
+{
+    public enum WishListItemCreateStatus
+    {
+        Created,
+        CardPrintingNotFound,
+        Duplicate
+    }
+
+    public class WishListItemCreateResult
+    {
+        public WishListItemCreateStatus Status { get; init; }
+
+        public WishListItemResponse? Item { get; init; }
+    }
+
+}
