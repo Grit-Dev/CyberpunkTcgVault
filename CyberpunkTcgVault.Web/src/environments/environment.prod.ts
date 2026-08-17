76ecb2ae26001fae4b98ff8443464277ab470d33
Paul McGinley <paulmcginley16@outlook.com>
2026-08-17 22:34:17 +0100

    dd

--- CyberpunkTcgVault.Web/src/environments/environment.prod.ts
+++ CyberpunkTcgVault.Web/src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
+// export const environment = {
+//   production: true,
+
+//   // Production API URL will be updated when the application is hosted.
+//   apiUrl: 'https://api.your-domain.com',
+// };
+
 export const environment = {
   production: true,
-
-  // Production API URL will be updated when the application is hosted.
-  apiUrl: 'https://api.your-domain.com',
+  apiUrl: 'https://choomvault-api-prod-h2arc9djbrdzb4cv.francecentral-01.azurewebsites.net',
 };
